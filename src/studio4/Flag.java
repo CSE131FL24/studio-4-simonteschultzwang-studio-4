78bdb63ad4277dc91b0188f1878204e5174a7bbb
allah <allah@10.232.190.32>
2024-10-02 13:58:48 -0500

    Studio 4 part 2

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledRectangle(0.25, 0.5, 0.25, 0.5);
 		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(0.75, 0.75, 0.25, 0.25);
+		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledRectangle(0.75, 0.25, 0.25, 0.25);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledEllipse(0.25, 0.5, 0.2, 0.4);
+		
+		StdDraw.setPenRadius(0.02);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(0.5, 0.5,0.5, 0.5);
 	}
+	
 }
